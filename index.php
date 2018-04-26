df963ef25052873e3bc437307e9beaf24fba4c29
Jose Hugo Cuaya Contreras <caitos_02@live.com.mx>
2018-04-25 21:10:52 -0500

    Unidad 3 Actividad 2

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
 require 'vendor/autoload.php';
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request; //Uso de interfaces PSR-7 para solicitudes, respuestas y transmisiones.
 
+$client = new GuzzleHttp\Client(['base_uri' => 'http://api.openweathermap.org/data/2.5/']); // Crea un cliente y proporciona una URL base
 
+$response = $client->request('GET', 'forecast?zip=72000,mx&appid=1af7288168a8759d536b862ca737da54');// Envio de solicitud a openweathermap
 
-$app = new \Slim\App;
+header("Content-type: application/json");// Da formato a la respuesta optenida
 
-$app->get('/clima', function (Request $request, Response $response, array $args) {
-    
-    $data = array('Nombre' => 'Hugo', 'No_Cuenta' => 414146208);
-    return $response->withJson($data);
-
-});
-$app->run();
+echo $response -> getBody();//salida del Json de la solicitud
